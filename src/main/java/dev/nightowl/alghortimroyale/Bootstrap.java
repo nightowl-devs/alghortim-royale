2f62699721f415213cb36a62ed6854d5cd5907a8
Stanisław Botwina <stanislaw.botwina@tkkom.local>
2025-09-02 15:49:32 +0200

    feat: added shutdown hook and module unloading

--- src/main/java/dev/nightowl/alghortimroyale/Bootstrap.java
+++ src/main/java/dev/nightowl/alghortimroyale/Bootstrap.java
@@ -4,7 +4,6 @@ import dev.nightowl.alghortimroyale.core.App;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
     App app = new App();
     app.start();
   }
--- src/main/java/dev/nightowl/alghortimroyale/core/App.java
+++ src/main/java/dev/nightowl/alghortimroyale/core/App.java
@@ -11,17 +11,60 @@ import java.util.ArrayList;
 public class App {
   private final ArrayList<Module> modules = new ArrayList<>();
 
+  private static App instance;
+
+  public App() {
+      instance = this;
+  }
+
+  public static App getInstance() {
+      return instance;
+  }
+
   public void start() {
     modules.add(new DNSModule());
     modules.add(new ProxyModule());
 
     loadModules();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> this.shutdown(true)));
   }
 
   private void loadModules() {
     for (Module module : modules) {
       Logger.log(LogLevel.INFO, "Loading module: " + module.getName());
       module.init();
+      module.running = true;
     }
   }
+
+  private void unloadModules() {
+
+      for (Module module : modules) {
+          if(!module.running) return;
+          Logger.log(LogLevel.INFO,"Unloading module: " + module.getName() );
+          module.shutdown();
+          module.running = false;
+      }
+  }
+
+
+
+  public void shutdown(Boolean graceful) {
+      unloadModules();
+
+      int status =0;
+      if (!graceful) {
+          Logger.log(LogLevel.ERROR, "The application has encountered an error while running...");
+          Logger.log(LogLevel.ERROR, "Read the above logs in order to resolve the issue!");
+          status = 1;
+      }
+
+      Logger.log(LogLevel.INFO, "Goodbye...");
+      System.exit(status);
+
+  }
+
+
+
 }
