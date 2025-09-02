c5d3cc977e0268210cdfa2307c4ee667a885cfb1
Stanisław Botwina <stanislaw.botwina@tkkom.local>
2025-09-02 15:53:05 +0200

    fix: change run configuration

--- src/main/java/dev/nightowl/alghortimroyale/Bootstrap.java
+++ src/main/java/dev/nightowl/alghortimroyale/Bootstrap.java
@@ -2,7 +2,7 @@ package dev.nightowl.alghortimroyale;
 
 import dev.nightowl.alghortimroyale.core.App;
 
-public class Main {
+public class Bootstrap {
   public static void main(String[] args) {
     App app = new App();
     app.start();
--- src/main/java/dev/nightowl/alghortimroyale/module/Module.java
+++ src/main/java/dev/nightowl/alghortimroyale/module/Module.java
@@ -3,6 +3,8 @@ package dev.nightowl.alghortimroyale.module;
 public abstract class Module {
   public abstract void init();
 
+  public boolean running = false;
+
   public abstract void shutdown();
 
   public abstract String getName();
